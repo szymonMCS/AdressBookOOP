9b1785cd83d4f672c1ba6640a2364eb6f52d624d
Szymon Maciejewski <szymon.maciejewski.programista@gmail.com>
2023-09-28 19:11:43 +0200

    Added load user to vector on start program

--- PlikZUzytkownikami.h
+++ PlikZUzytkownikami.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cstdlib>
 
 #include "Uzytkownik.h"
 #include "MetodyPomocnicze.h"
@@ -15,10 +16,12 @@ class PlikZUzytkownikami{
 
     bool czyPlikJestPusty();
     std::string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+    Uzytkownik pobierzDaneUzytkownika(std::string daneJednegoUzytkownikaOddzielonePionowymiKreskami);
 
 public:
     PlikZUzytkownikami();
     void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+    std::vector <Uzytkownik>  wczytajUzytkownikowZPliku();
 };
 
 #endif // PLIKZUZYTKOWNIKAMI_H
--- UzytkownikMenadzer.cpp
+++ UzytkownikMenadzer.cpp
@@ -1,7 +1,18 @@
 #include "UzytkownikMenadzer.h"
 
-Uzytkownik UzytkownikMenadzer::podajDaneNowegoUzytkownika()
-{
+void UzytkownikMenadzer::rejestracjaUzytkownika(){
+
+    Uzytkownik uzytkownik = podajDaneNowegoUzytkownika();
+
+    uzytkownicy.push_back(uzytkownik);
+    plikZUzytkownikami.dopiszUzytkownikaDoPliku(uzytkownik);
+
+    std::cout << std::endl << "Konto zalozono pomyslnie" << std::endl << std::endl;
+    system("pause");
+}
+
+Uzytkownik UzytkownikMenadzer::podajDaneNowegoUzytkownika(){
+
     Uzytkownik uzytkownik;
 
     uzytkownik.ustawId(pobierzIdNowegoUzytkownika());
@@ -18,18 +29,19 @@ Uzytkownik UzytkownikMenadzer::podajDaneNowegoUzytkownika()
     return uzytkownik;
 }
 
-int UzytkownikMenadzer::pobierzIdNowegoUzytkownika()
-{
+int UzytkownikMenadzer::pobierzIdNowegoUzytkownika(){
+
     if (uzytkownicy.empty() == true)
         return 1;
     else
         return uzytkownicy.back().pobierzId() + 1;
 }
 
-bool UzytkownikMenadzer::czyIstniejeLogin(std::string login)
-{
+bool UzytkownikMenadzer::czyIstniejeLogin(std::string login){
+
     for (int i = 0; i < uzytkownicy.size(); i++){
         if (uzytkownicy[i].pobierzLogin() == login){
+
             std::cout << std::endl << "Istnieje uzytkownik o takim loginie." << std::endl;
             return true;
         }
@@ -37,22 +49,18 @@ bool UzytkownikMenadzer::czyIstniejeLogin(std::string login)
     return false;
 }
 
-void UzytkownikMenadzer::rejestracjaUzytkownika(){
-
-    Uzytkownik uzytkownik = podajDaneNowegoUzytkownika();
-
-    uzytkownicy.push_back(uzytkownik);
-    plikZUzytkownikami.dopiszUzytkownikaDoPliku(uzytkownik);
-
-    std::cout << std::endl << "Konto zalozono pomyslnie" << std::endl << std::endl;
-    system("pause");
-}
-
 void UzytkownikMenadzer::wypiszWszystkichUzytkownikow(){
 
     for (int i = 0; i < uzytkownicy.size(); i++){
+
             std::cout << uzytkownicy[i].pobierzId() << std::endl;
             std::cout << uzytkownicy[i].pobierzLogin() << std::endl;
             std::cout << uzytkownicy[i].pobierzHaslo() << std::endl;
     }
 }
+
+void UzytkownikMenadzer::wczytajUzytkownikowZPliku(){
+
+    uzytkownicy = plikZUzytkownikami.wczytajUzytkownikowZPliku();
+}
+
